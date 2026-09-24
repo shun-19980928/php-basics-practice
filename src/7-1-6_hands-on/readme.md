0542908208154ee3bcd363a90b346bce11dd6ca5
shun19980928 <shun_dora@icloud.com>
2026-09-25 00:00:03 +0900

    feat: 商品価格計算プログラムとREADMEを追加

---
+++ src/7-1-6_hands-on/readme.md
@@ -0,0 +1,12 @@
+## 概要
+COACHTECH 教材 Tutorial 7-1「PHPの基礎 ハンズオン演習」で作成した成果物です。
+
+## 使用技術
+- PHP
+
+## 学んだこと
+phpの変数の定義や計算処理のコマンドの使いかた
+HTMLと合わせて使うと見やすくなる
+
+## 動作確認
+http://localhost:8000/7-1-6_hands-on/practice/price_calculator.php
---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>はじめてのPHP</title>
+</head>
+<body>
+    <h1>PHPの世界へようこそ！</h1>
+    <p>PHPを使って、動的なコンテンツを表示してみましょう。</p>
+    <p>1 + 1 は、<?php echo 1 + 1; ?> です。</p>
+    <p>現在の日時は、<?php echo date("Y年m月d日 H時i分s秒"); ?> です。</p>
+</body>
+</html>
